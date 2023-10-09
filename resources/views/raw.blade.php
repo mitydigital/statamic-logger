31a5c08f6ef3482a25dac2722abeb93ca6673cc4
Marty Friedel <1491079+martyf@users.noreply.github.com>
2023-10-10 09:46:14 +1100

    Tweak font sizes

--- resources/views/raw.blade.php
+++ resources/views/raw.blade.php
@@ -1,4 +1,4 @@
-<div class="mt-2">
+<div class="text-xs mt-2">
     <div><strong>{{ __('statamic-logger::utility.raw') }}:</strong></div>
-    <pre class="text-xs">{{ print_r($data, true) }}</pre>
+    <pre>{{ print_r($data, true) }}</pre>
 </div>
